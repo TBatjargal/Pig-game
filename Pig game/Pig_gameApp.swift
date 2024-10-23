ecb234feccaf4d8462c96961f50def196ad384cb
Tsolmon Batjargal <b.tsolmon0421@icloud.com>
2024-10-23 10:06:31 -0500

    Initial Commit

---
+++ Pig game/Pig_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pig_gameApp.swift
+//  Pig game
+//
+//  Created by Tsolmon Batjargal on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Pig_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
